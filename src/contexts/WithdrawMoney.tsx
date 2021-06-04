245f401d082b317baea3f65040897ad545e467e9
Alex <alexoliveira604@gmail.com>
2021-06-04 06:42:00 -0300

    WithdrawMoney context was been created

---
+++ src/contexts/WithdrawMoney.tsx
@@ -0,0 +1,57 @@
+import { createContext, Dispatch, ReactNode, useContext, useEffect, useState } from "react";
+import { useMensages } from "../contexts/mapMensages";
+import { api } from "../pages/api/api";
+
+type WithdrawMoneyData = {
+    newWithdrawMoney: () => void;
+};
+
+export const WithdrawMoney = createContext({} as WithdrawMoneyData)
+
+type WithdrawMoneyProps = {
+    children: ReactNode;
+}
+
+export const WithdrawMoneyProvider = ({ children }: WithdrawMoneyProps) => {
+    const { msg, allMsg, setAllMsg, msgsCounter, setInputMsg} = useMensages()    
+
+    useEffect(() => {
+        if (allMsg[allMsg.length -2] == "How much can you to deposit?") {
+            sendData();
+        }
+    }, [msgsCounter])
+
+    const sendData = () => {
+        if (msg[msg] != 0) {
+            api.post(`/users/${user.id}`, { withdrawValue: msg });
+            api.get(`/users/${user.id}`)
+                .then(res => {     
+                    setAllMsg([
+                        allMsg,
+                        `${res.coinIcon} ${msg} was been withdraw of your wallet`,
+                    ])
+                    setInputMsg("text")
+                })
+        }
+    }
+
+    const newWithdrawMoney = () => {
+        setAllMsg([
+            allMsg,
+            "How much money do you want to withdraw?",
+        ])
+        setInputMsg("number")
+    }
+
+    return (
+        <WithdrawMoney.Provider value={{
+            newWithdrawMoney,
+        }}>
+            {children}
+        </WithdrawMoney.Provider>
+    )
+}
+
+export const useWithdrawMoney = () => {
+    return useContext(WithdrawMoney);
+}
