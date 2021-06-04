f8b41cfba51fbc917a04d25c37dc1cf26e66bd77
Alex <alexoliveira604@gmail.com>
2021-06-04 06:41:22 -0300

    deposit context was been created

---
+++ src/contexts/Deposit.tsx
@@ -0,0 +1,57 @@
+import { createContext, Dispatch, ReactNode, useContext, useEffect, useState } from "react";
+import { useMensages } from "../contexts/mapMensages";
+import { api } from "../pages/api/api";
+
+type DepositData = {
+    newDeposit: () => void;
+};
+
+export const Wallet = createContext({} as DepositData)
+
+type DepositProps = {
+    children: ReactNode;
+}
+
+export const DepositProvider = ({ children }: DepositProps) => {
+    const { msg, allMsg, setAllMsg, msgsCounter, setInputMsg} = useMensages()    
+
+    useEffect(() => {
+        if (allMsg[allMsg.length -2] == "How much can you to deposit?") {
+            sendData();
+        }
+    }, [msgsCounter])
+
+    const sendData = () => {
+        if (msg[msg] != 0) {
+            api.post(`/users/${user.id}`, { depositValue: msg });
+            api.get(`/users/${user.id}`)
+                .then(res => {     
+                    setAllMsg([
+                        allMsg,
+                        `${res.coinIcon} ${msg} was been deposited in your wallet`,
+                    ])
+                    setInputMsg("text")
+                })
+        }
+    }
+
+    const newDeposit = async () => {
+        await setAllMsg([
+            allMsg,
+            "How much can you to deposit?",
+        ])
+        setInputMsg("number")
+    }
+
+    return (
+        <Wallet.Provider value={{
+            newDeposit,
+        }}>
+            {children}
+        </Wallet.Provider>
+    )
+}
+
+export const useDeposit = () => {
+    return useContext(Wallet);
+}
