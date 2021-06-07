cc6067b98a70e55a8d0b766daccb3beeda1aca51
Alex <alexoliveira604@gmail.com>
2021-06-07 19:39:49 -0300

    routes update

--- api/api/routes/api-routes.js
+++ api/api/routes/api-routes.js
@@ -7,16 +7,37 @@ router.get('/', function (req, res) {
         message: 'Welcome to RESTHub crafted with love!',
     });
 });
-// Import contact controller
-var controler = require('../controllers/Controller');
-// Contact routes
+
+// All Accounts
+const allAccounts = require('../controllers/allAccounts');
+// CRUD
+const login = require('../controllers/login');
+const newAccount = require('../controllers/newAccount');
+const update = require('../controllers/update');
+const deleteAccount = require('../controllers/delete');
+// transactions
+const transactions = require('../controllers/transactions');
+// wallet
+const wallet = require('../controllers/wallet');
+// to send money
+const sendMoney = require('../controllers/sendMoney');
+
+// User routes
 router.route('/users')
-    .get(controler.index)
-    .post(controler.new);
+    .get(allAccounts.index)
+    .post(newAccount.new);
 router.route('/users/:user_email')
-    .post(controler.view)
-    .patch(controler.update)
-    .put(controler.update)
-    .delete(controler.delete);
+    .post(login.login)
+    .patch(update.update)
+    .put(update.update)
+    .delete(deleteAccount.delete);
+router.route('/users/trasactions/:user_email')
+    .get(transactions.transactions)
+    .post(transactions.newTransactions);
+router.route('/users/wallet/:user_email')
+    .get(wallet.wallet)
+    .post(wallet.newCoin);
+router.route('/users/sendMoney/:user_email')
+    .post(sendMoney.sendMoney);
 // Export API routes
 module.exports = router;
